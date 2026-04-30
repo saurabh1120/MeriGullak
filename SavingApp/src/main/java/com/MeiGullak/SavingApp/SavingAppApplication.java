d900f099116e5136c655b5bc1edaccd41266ecf1
saurabh1120 <saurabhsinha1850@gmail.com>
2026-04-30 14:36:43 +0530

    Initial commit for MeriGullak

---
+++ SavingApp/src/main/java/com/MeiGullak/SavingApp/SavingAppApplication.java
@@ -0,0 +1,13 @@
+package com.MeiGullak.SavingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingAppApplication.class, args);
+	}
+
+}
---
+++ SavingApp/src/test/java/com/MeiGullak/SavingApp/SavingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MeiGullak.SavingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
